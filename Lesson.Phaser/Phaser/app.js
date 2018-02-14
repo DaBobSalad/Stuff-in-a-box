e2714a10b6279a95393eacbaa5547b8f9ca844a0
DaBobSalad <Tsangpros@gmail.com>
2018-02-14 19:10:56 +0800

    lesson.Phaser

--- Lesson.Phaser/Phaser/app.js
+++ Lesson.Phaser/Phaser/app.js
@@ -6,8 +6,8 @@ function preload(){
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('ground', 'assets/platforms.png');
 	game.load.image('star', 'assets/star.png');
-	game.load.spritesheet('dude', 'assets/dude', 32, 48);
-	game.load.spritesheet('dog', 'assets/baddie', 32, 32)
+	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+	game.load.spritesheet('dog', 'assets/baddie.png', 32, 32)
 }
 
 function create(){
