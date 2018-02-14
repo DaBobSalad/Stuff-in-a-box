0c596c2a90eec1d2ad0fef87db759d55280fe6e2
DaBobSalad <Tsangpros@gmail.com>
2018-02-14 19:11:47 +0800

    lesson.Phaser

--- Lesson.Phaser/Phaser/app.js
+++ Lesson.Phaser/Phaser/app.js
@@ -4,7 +4,7 @@ var lives = 1.5;
 
 function preload(){
 	game.load.image('sky', 'assets/sky.png');
-	game.load.image('ground', 'assets/platforms.png');
+	game.load.image('ground', 'assets/platform.png');
 	game.load.image('star', 'assets/star.png');
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	game.load.spritesheet('dog', 'assets/baddie.png', 32, 32)
