ba0fee8cf223da3b3f50eed03a10c607a772f985
DaBobSalad <Tsangpros@gmail.com>
2018-03-14 18:45:53 +0800

    hoi

--- Lesson.Phaser/Phaser/app.js
+++ Lesson.Phaser/Phaser/app.js
@@ -8,7 +8,7 @@ function preload(){
 	game.load.image('star', 'assets/star.png');
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	game.load.spritesheet('dog', 'assets/baddie.png', 32, 32);
-	game.load.image('firstaid'  'assets/firstaid.png');
+	game.load.image('firstaid' , 'assets/firstaid.png');
 }
 
 function create(){
